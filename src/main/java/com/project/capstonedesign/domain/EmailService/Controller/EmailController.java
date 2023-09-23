85e262eb4cd63447a1e42d2f59b6f400ad22881c
EnoughKK <ksu8063@naver.com>
2023-09-24 04:00:49 +0900

    이메일 인증 구현 완료

--- src/main/java/com/project/capstonedesign/domain/EmailService/Controller/EmailController.java
+++ src/main/java/com/project/capstonedesign/domain/EmailService/Controller/EmailController.java
@@ -1,22 +1,35 @@
 package com.project.capstonedesign.domain.EmailService.Controller;
 
 import com.project.capstonedesign.domain.EmailService.Service.EmailService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequiredArgsConstructor
 public class EmailController {
+
     private final EmailService emailService;
 
-    @PostMapping("/emailConfirm")
-    public String emailConfirm(@RequestParam("email") String email) throws Exception {
+    @PostMapping("login/mailConfirm")
+    public String mailConfirm(@RequestBody EmailAuthRequestDto emailDto) throws MessagingException, UnsupportedEncodingException {
 
-        String confirm = emailService.sendSimpleMessage(email);
-
-        return confirm;
+        String authCode = emailService.sendEmail(emailDto.getEmail());
+        return authCode;
     }
 
+    @Data
+    public static class EmailAuthRequestDto {
+
+        @NotEmpty(message = "이메일을 입력해주세요")
+        public String email;
+    }
 }
